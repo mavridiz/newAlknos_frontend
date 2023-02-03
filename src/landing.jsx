15b9a46b00ad76a0d29d8265b48571e4df0b94ff
mavridiz <mavridis.gutierrez.sebastian@gmail.com>
2023-02-03 08:30:39 -0600

    Segundo Commit

--- src/landing.jsx
+++ src/landing.jsx
@@ -4,7 +4,28 @@ import { Canvas } from '@react-three/fiber'
 import { Float } from '@react-three/drei'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import React, { Suspense } from "react";
+import { GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
 
+const features = [
+  {
+    name: 'Competitive exchange rates',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: GlobeAltIcon,
+  },
+  {
+    name: 'No hidden fees',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ScaleIcon,
+  },
+  {
+    name: 'Transfers are instant',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: LightningBoltIcon,
+  },
+]
 
 
 export default function Contactanos() {
@@ -77,11 +98,38 @@ export default function Contactanos() {
           
         </Suspense>
               </Canvas>
+              
             </div>
+            
           </div>
         </div>
+        
       </div>
+
+      <div className="py-12 bg-transparent">
+      <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h3 className="font-poppins text-transparent text-sm bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">I A &nbsp; P O W E R E D</h3>
+        <h1 className="font-raleway text-5xl text-white">Detección de fórmulas química.</h1> 
+<div className="py-5">
+<p className="font-raleway text-gray-400 font-bold font-base">Nuestro nuevo sistema de visión artificial permite detectar compuestos tanto inorgánicos como orgánicos</p>
+</div>
+        <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-8">
+          {features.map((feature) => (
+            <div key={feature.name}>
+              <dt>
+                <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <feature.icon className="h-6 w-6" aria-hidden="true" />
+                </div>
+                <p className="mt-5 text-lg leading-6 font-medium text-white">{feature.name}</p>
+              </dt>
+              <dd className="mt-2 text-base text-gray-200">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
       </div>
+    </div>
+      </div>
+      
     </div>
     )
   }
